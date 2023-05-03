9629b1a5c80ffeba1e1457365af412ceaff2bd3a
Mide1020 <akinrefonayomide1@gmail.com>
2023-05-03 08:50:27 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return; 1
+ */
+
+int main(void)
+{
+	putchar("and that piece of art is useful" - "dora kpora, 2015-10-19\n");
+	return(1);
+}
