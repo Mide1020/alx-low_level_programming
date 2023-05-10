c4cbd9db46465938313baad7f221873ed4c6436a
Mide1020 <akinrefonayomide1@gmail.com>
2023-05-10 05:45:58 -0700

    101-crackme_password

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -3,16 +3,8 @@
  * Double pointer
  * writting functions that set the value of a pointer
  * prototypes
- * Return - 0
+ * Return 0
  */
-int main(void)
-{
-	void set_string(char **s, char *to);
-	char *s0 = "Mide";
-	char *s1 = "Esther";
 
-	printf ("%s, %s\n", s0, s1);
-	set_string (&s1, s0);
-	printf ("%s, %s\n", s0 ,s1);
-	return (0);
-}
+void set_string(char **s, char *to)
+int main  	
