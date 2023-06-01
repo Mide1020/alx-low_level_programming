5755e30540e23b354c7831c0be30ae464422d4e1
mide1020 <akinrefonayomide1@gmail.com>
2023-06-01 16:40:49 -0700

    1-- first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void first(void);
+
+/**
+ * first - write out a sentence.
+ * Execute thr function
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
