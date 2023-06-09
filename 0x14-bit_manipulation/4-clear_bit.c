e8269d57c89403006e30f41765688f43f8c6d941
mide1020 <akinrefonayomide1@gmail.com>
2023-06-09 11:29:54 -0700

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
 /**
  * clearing_bit - setting the value of the bit
  * @n: pointer of decimal we need to changed
  * @index: index for changing position
+ * Return: if it work return 1, else return -1
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
@@ -12,7 +12,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index > 64)
 		return (-1);
 	h = index;
-	for (x = 1; hold > 0; x *= 2, h--)
+	for (x = 1; h > 0; x *= 2, h--)
 		;
 
 	if ((*n >> index) & 1)
